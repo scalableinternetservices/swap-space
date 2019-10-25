2b146ead4e7b30eab41787fba631575b9152bc02
Galen Wong <wonggalen1999@gmail.com>
2019-10-24 19:57:08 -0700

    Added user sessions and complete sign in GUI (#9)

--- environment/swapspace/app/controllers/users_controller.rb
+++ environment/swapspace/app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # handle a successful save.
+      log_in @user
       flash[:success] = 'Welcome to SwapSpace!'
       redirect_to @user
     else
