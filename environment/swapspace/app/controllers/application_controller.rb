ba692164b2d0d3e4c6ae640ad9784b35691dda28
GalenWong <wonggalen1999@gmail.com>
2019-10-25 00:46:53 -0400

    login control for creating items (testing failing)

--- environment/swapspace/app/controllers/application_controller.rb
+++ environment/swapspace/app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  private
+    # confirm currently user is logged in
+    def logged_in_user
+      unless logged_in?
+        # return to current page after logging in
+        # store_locatiod
+        flash[:danger] = 'Please Log in'
+        redirect_to login_url
+      end
+    end
 end
--- environment/swapspace/app/controllers/items_controller.rb
+++ environment/swapspace/app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:create, :edit, :update, :destroy]
 
   # GET /items
   # GET /items.json
