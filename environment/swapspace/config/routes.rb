67880fb25b2bbb5ac477eab9182aa8068582e20f
GalenWong <wonggalen1999@gmail.com>
2019-10-24 22:34:55 -0400

    Added user sessions and complete sign in GUI

--- environment/swapspace/config/routes.rb
+++ environment/swapspace/config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   resources :users
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
