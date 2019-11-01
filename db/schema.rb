f16f77f8448183ae2bbb6285bc129c653cf1f96b
GalenWong <wonggalen1999@gmail.com>
2019-11-01 12:38:07 -0700

    Added item description field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191025030044) do
+ActiveRecord::Schema.define(version: 20191101193055) do
 
   create_table "items", force: :cascade do |t|
     t.text "category"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20191025030044) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
