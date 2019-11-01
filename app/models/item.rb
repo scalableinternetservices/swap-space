1a7ea3d6e4b4cac4bb0dcf6277ea3155b760759d
GalenWong <wonggalen1999@gmail.com>
2019-11-01 14:28:31 -0700

    Added join table to relate trades

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
     belongs_to :user
+    has_and_belongs_to_many :trades,
+        class_name: 'Item',
+        join_table: :trades,
+        foreign_key: :item_id,
+        association_foreign_key: :bid_by_item_id
 end
