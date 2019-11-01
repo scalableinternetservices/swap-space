7cf79ef1c0d09409c993f745743aa2dc85ee1680
GalenWong <wonggalen1999@gmail.com>
2019-11-01 12:58:27 -0700

    Log in redirect works now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to session[:forwarding_url] || user
+      redirect_to session.delete(:forwarding_url) || user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
