01c23e129ec1f3263d79f5fab5273515499e6d9f
cheimu <xerhoneyfc@gmail.com>
2019-11-05 15:11:27 -0800

    add trade established field in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191101210516) do
+ActiveRecord::Schema.define(version: 20191105230555) do
 
   create_table "items", force: :cascade do |t|
     t.text "category"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20191101210516) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.boolean "trade_established", default: false
   end
 
   create_table "trades", id: false, force: :cascade do |t|
