241d7676c62e3a0ef33138b36c761b505e2d0374
rsukumar75 <rish.sukumar@gmail.com>
2019-11-22 15:42:41 -0800

    Added seeding file, .ebextension seed.config, and added description field for all item cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+dummyItem = ["Furniture", "Chair", "Dummy Chair"]
+
+for x in 1...100
+    randEmail = Array.new(10){[*"A".."Z", *"0".."9"].sample}.join + "@abc.com"
+    User.create(name: "dummyUser#{x}", email: randEmail, password: "hello123")
+end
+
+for x in 1...100
+    randID = rand(1..100)
+    i = Item.create(category: dummyItem[0], name: dummyItem[1], user_id: randID, description: dummyItem[2])
+    i.images.attach(io: File.open(File.join(Rails.root, 'app','assets','images', 'Chair.jpeg')), filename: 'dummyChair.jpg')
+end
