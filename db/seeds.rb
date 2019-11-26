83e2a584964f1e621780e4072b1ad71816aea69a
GalenWong <wonggalen1999@gmail.com>
2019-11-26 00:13:03 -0800

    Faster seeding file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,32 @@
 
 dummyItem = ["Furniture", "Chair", "Dummy Chair"]
 
+# for faster generation
+# you cannot login as seeded users!
+BCrypt::Engine.cost=(1)
+dummyUsers = []
 for x in 1...2000
-    randEmail = Array.new(10){[*"A".."Z", *"0".."9"].sample}.join + "@abc.com"
-    User.create(name: "dummyUser#{x}", email: randEmail, password: "hello123")
+    randEmail = Array.new(10){[*"A".."Z", *"0".."9"].sample}.join + "#{x}" + "@abc.com"
+    user = User.new(name: "dummyUser#{x}", email: randEmail, password: 'hello123')
+    dummyUsers << user
+    print "\r #{x} users created"
 end
+puts ''
+puts 'dummy users created'
+User.import dummyUsers
+puts 'dummy users imported'
 
+items = []
+image_fid = File.open(File.join(Rails.root, 'app','assets','images', 'Chair.jpeg'))
 for x in 1...2000
     randID = rand(1..100)
-    i = Item.create(category: dummyItem[0], name: dummyItem[1], user_id: randID, description: dummyItem[2])
-    i.images.attach(io: File.open(File.join(Rails.root, 'app','assets','images', 'Chair.jpeg')), filename: 'dummyChair.jpg')
+    i = Item.new(category: dummyItem[0], name: dummyItem[1], user_id: randID, description: dummyItem[2])
+    i.images.attach(io: image_fid, filename: 'dummyChair.jpg')
+    image_fid.seek(0, IO::SEEK_SET)
+    i.save
+    print "\r #{x} items created"
 end
+
+puts ''
+puts 'dummy items created'
+puts 'dummy items imported'
