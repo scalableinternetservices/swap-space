98c6cc200abbf030e61f78286fc63e2cadcfc214
GalenWong <wonggalen1999@gmail.com>
2019-12-01 17:00:52 -0800

    Upgrade to rails 6

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  connects_to database: { writing: :primary, reading: :primary_replica }
+  if Rails.env.production?
+    connects_to database: { writing: :primary, reading: :primary_replica }
+  end
 end
